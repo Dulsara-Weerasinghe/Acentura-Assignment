9e2a6ca7aaca61aba19864e68aedd80acbb13ec2
dulsara_w <dulsara_w@epiclanka.net>
2025-05-18 17:11:24 +0530

    Annotations added

--- src/main/java/com/example/eventManagement/config/WebConfig.java
+++ src/main/java/com/example/eventManagement/config/WebConfig.java
@@ -2,9 +2,10 @@ package com.example.eventManagement.config;
 
 import com.example.eventManagement.domain.RateLimitInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private RateLimitInterceptor rateLimitInterceptor;
